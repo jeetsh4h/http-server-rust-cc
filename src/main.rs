261d5e963d7a3a4247d11af6c352457583534696
JeetShah <jeetsh4h@gmail.com>
2024-05-07 00:11:19 +0530

    2nd stage pass?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -10,17 +10,44 @@ fn respond_202(stream: &mut TcpStream) {
     };
 }
 
+fn respond_404(stream: &mut TcpStream) {
+    match stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n") {
+        Ok(size) => println!("Sent {size} bytes"),
+        Err(err) => println!("error writing to stream: {err}"),
+    };
+}
+
+fn parse_path(http_response: &String) -> String {
+    let whitespace_split_response: Vec<&str> = http_response.split_whitespace().collect();
+    return whitespace_split_response[1].to_string();
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let mut incoming_buf_as_str = String::new();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
-                respond_202(&mut stream);
+
+                match _stream.read_to_string(&mut incoming_buf_as_str) {
+                    Ok(size) => println!("Received {size} bytes"),
+                    Err(err) => {
+                        println!("error reading from stream: {err}");
+                        continue;
+                    }
+                }
+                let path = parse_path(&incoming_buf_as_str);
+                if path == "/" {
+                    respond_404(&mut _stream)
+                } else {
+                    respond_202(&mut _stream);
+                }
+
             }
             Err(err) => {
                 println!("error: {err}");
