ab3de06a33820ee37a6b161b80cd4b135b533326
JeetShah <jeetsh4h@gmail.com>
2024-05-07 15:53:46 +0530

    7th stage v0

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use httparse::{Request, EMPTY_HEADER};
+use std::error::Error;
 
 fn respond_202(stream: &mut TcpStream) {
     match stream.write(b"HTTP/1.1 200 OK\r\n\r\n") {
@@ -17,25 +17,15 @@ fn respond_404(stream: &mut TcpStream) {
     };
 }
 
-fn parse_path(http_response: &String) -> String {
-    let whitespace_split_response: Vec<&str> = http_response.split_whitespace().collect();
+fn respond_202_body(stream: &mut TcpStream, body: &str) {
+    let body_len = body.len();
 
-    // this assumes that I will always receive a valid HTTP request
-    // I am only returning a path
-    return whitespace_split_response[1].to_string();
-}
-
-fn echo_respond(stream: &mut TcpStream, echo_string: &String ) {
     let status_line = "HTTP/1.1 200 OK\r\n";
     let content_type = "Content-Type: text/plain\r\n";
-    let content_length = format!("Content-Length: {}\r\n\r\n", echo_string.len());
-    let echo_fmt = format!("{}", echo_string);
+    let content_length = format!("Content-Length: {}\r\n", body_len);
+    let body = format!("\r\n{}", body);
 
-    let mut response = String::new();
-    response.push_str(status_line);
-    response.push_str(content_type);
-    response.push_str(&content_length);
-    response.push_str(&echo_fmt);
+    let response = format!("{}{}{}{}", status_line, content_type, content_length, body);
 
     match stream.write(response.as_bytes()) {
         Ok(size) => println!("Sent {size} bytes"),
@@ -43,50 +33,78 @@ fn echo_respond(stream: &mut TcpStream, echo_string: &String ) {
     }
 }
 
+fn respond_echo(stream: &mut TcpStream, echo_path: &str) {
+    let path_parsed: Vec<&str> = echo_path.split('/').collect();
+    assert_eq!(path_parsed[1], "echo");
+    assert!(path_parsed.len() >= 3);
+
+    let echo_string = path_parsed[2];
+    respond_202_body(stream, echo_string);
+}
+
+fn parse_request_path(buf: &[u8]) -> Result<String, Box<dyn Error>> {
+    let mut headers = [ EMPTY_HEADER; 4 ];
+    let mut req = Request::new(&mut headers);
+    match req.parse(buf) {
+        Err(e) => Err(Box::new(e)),
+        Ok(_body_offset) => {
+            let path = req.path.unwrap();
+            Ok(path.to_string())
+        }
+    }
+}
+
+fn respond_user_agent(stream: &mut TcpStream, buf: &[u8]) {
+    let mut headers = [ EMPTY_HEADER; 4 ];
+    let mut req = Request::new(&mut headers);
+
+    match req.parse(buf) {
+        Ok(_body_offset) => {
+            let user_agent_header = req.headers.iter().find(|h| h.name == "User-Agent");
+            match user_agent_header {
+                Some(header) => {
+                    unsafe {
+                        let body = std::str::from_utf8_unchecked(header.value);
+                        respond_202_body(stream, body);
+                    }
+                },
+                None => println!("error: user-agent header not present"),
+            }
+        }
+        Err(e) => println!("error parsing headers: {}", e),
+    }
+}
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    let mut buffer = [0; 1024];
 
     for stream in listener.incoming() {
+        let mut buf = [0; 1024];
+
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                
-                // recv1024 function
-                match _stream.read(&mut buffer) {
-                    Ok(size) => println!("Received {size} bytes"),
-                    Err(err) => {
-                        println!("error reading from stream: {err}");
-                        continue;
-                    }
-                }
-                
-                // parse the buffer, turn into the string, then give a response accordingly 
-                let buf_as_str = unsafe { std::str::from_utf8_unchecked(&buffer) };
-                let path = parse_path(&buf_as_str.to_string());
-                if path == "/" {
-                    respond_202(&mut _stream);
-                } else {
-                    // further parse the path that results after parsing the buffer,
-                    // give response accordingly
-                    let path_parsed: Vec<&str> = path.split('/').collect();
-                    if path_parsed[1].to_string() == "echo" {
-                        echo_respond(&mut _stream, &path_parsed[2].to_string());
-                    } else {
-                        // not an echo command
-                        respond_404(&mut _stream);
-                    }
-                }
-                
-                
+                // recv function,,, buffer size is one byte
+                match _stream.read(&mut buf) {
+                    Ok(size) => {
+                        println!("Received {} bytes", size);
 
+                        // parse path from request and call proper function
+                        match parse_request_path(&buf) {
+                            Ok(path) => match path.as_str() {
+                                "/" => respond_202(&mut _stream),
+                                r"/echo/*" => respond_echo(&mut _stream, &path),
+                                "/user-agent" => respond_user_agent(&mut _stream, &buf),
+                                _ => respond_404(&mut _stream),
+                            },
+                            Err(e) => println!("error parsing: {}", e),
+                        }
+                    }
+                    Err(e) => println!("error receiving: {}", e),
+                }
             }
-            Err(err) => {
-                println!("error: {err}");
-            }
+            Err(e) => println!("error connecting: {}", e),
         }
     }
 }
